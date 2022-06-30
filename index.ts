a3f40da4ecab78abfd8863e84f7b62669e51e045
ShinEunJin <sineun5501@naver.com>
2022-06-30 23:15:42 +0900

    dotenv 바로 적용되도록 index 처리

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 import 'module-alias/register';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import app from './app';
 import './db';
-
-dotenv.config();
 
 declare const process: {
   env: {
     PORT: number;
   };
 };
+
+(function () {
+  const result = dotenv.config({ path: path.join(__dirname, '.', '.env') });
+  if (result.parsed === undefined) {
+    throw new Error('❌ Cannot loaded environment variables file');
+  }
+})();
 
 app.listen(process.env.PORT, () =>
   console.log(`✅ server is connected to ${process.env.PORT}`)
