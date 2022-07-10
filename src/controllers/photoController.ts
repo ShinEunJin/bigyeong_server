bc41ecd1625282270da5451966e14f299eaa35c2
ShinEunJin <sineun5501@naver.com>
2022-07-10 21:39:12 +0900

    getphoto 로직

--- src/controllers/photoController.ts
+++ src/controllers/photoController.ts
@@ -1,5 +1,6 @@
 import Photo from '@/models/Photo.model';
 import { ObjectId } from 'mongoose';
+import { Request, Response } from 'express';
 
 interface PhotoType {
   __v: number;
@@ -14,7 +15,13 @@ interface PhotoType {
   updatedAt: string;
 }
 
-export const uploadPhoto = async (req: any, res: any) => {
+interface File {
+  file: {
+    location: string;
+  };
+}
+
+export const uploadPhoto = async (req: any, res: Response) => {
   try {
     const { location } = req.file;
     const { data } = req.body;
@@ -27,7 +34,7 @@ export const uploadPhoto = async (req: any, res: any) => {
   }
 };
 
-export const getPhotos = async (req: any, res: any) => {
+export const getPhotos = async (req: Request, res: Response) => {
   try {
     const { category } = req.query;
     const photos: PhotoType[] = await Photo.find({ category });
@@ -38,6 +45,19 @@ export const getPhotos = async (req: any, res: any) => {
     }
   } catch (error) {
     console.log('photoController getPhotos');
+    return { error };
+  }
+};
+
+export const getPhoto = async (req: Request, res: Response) => {
+  try {
+    const { id, category } = req.query;
+    const photo: PhotoType | null = await Photo.findOne({ _id: id, category });
+    if (photo) {
+      return res.json(photo);
+    }
+  } catch (error) {
+    console.log('photoController getPhoto');
     return { error };
   }
 };
--- src/router/photoRouter.ts
+++ src/router/photoRouter.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import routes from './routes';
 import upload from '@/middlewares/multer';
-import { getPhotos, uploadPhoto } from '@/controllers/photoController';
+import {
+  getPhoto,
+  getPhotos,
+  uploadPhoto,
+} from '@/controllers/photoController';
 
 const router = express.Router();
 
 router.get(routes.photos, getPhotos);
 
+router.get(routes.photo, getPhoto);
 router.post(routes.photo, upload.single('photo'), uploadPhoto);
 
 export = router;
