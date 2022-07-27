59616c6f810a3deba83568ccdf4354fc86f5c7f5
ShinEunJin <sineun5501@naver.com>
2022-07-27 23:30:37 +0900

    cors 적용

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+
 import router from './src/router';
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
