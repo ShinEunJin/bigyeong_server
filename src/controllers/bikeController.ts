e50fa225c28480c00ef3ff0ef5b0bbc0add6b7ac
ShinEunJin <sineun5501@naver.com>
2022-09-07 15:34:50 +0900

    getBikes controller 쿼리 변경

--- src/controllers/bikeController.ts
+++ src/controllers/bikeController.ts
@@ -14,14 +14,12 @@ interface BikeType {
 
 export const getBikes = async (req: Request, res: Response) => {
   try {
-    const { maker, model, genre } = req.query;
-    console.log({ maker, model, genre });
-    let body: any = {};
-    if (maker) body['maker'] = maker;
-    if (model) body['model'] = model;
-    if (genre) body['genre'] = genre;
+    const query = req.query;
+    let body = {};
+    for (const [key, val] of Object.entries(query)) {
+      if (val) Object.assign(body, { [key]: val });
+    }
     const bikes: BikeType[] = await Bike.find(body);
-    console.log('b', bikes);
     if (bikes.length > 0) return res.send(bikes);
     else return res.json(null);
   } catch (error) {
